622d226f96d71a1f2a2da2b526283998bbe3dd2d
kenjis <kenji.uui@gmail.com>
2021-01-06 20:17:44 +0900

    chore: create project
    composer create-project koriym/php-skeleton:1.1.6 ci3-to-4-migration-helper

---
+++ src/Ci3To4MigrationHelper.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\Ci3To4MigrationHelper;
+
+final class Ci3To4MigrationHelper
+{
+}
---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\Ci3To4MigrationHelper\Exception;
+
+class RuntimeException extends \RuntimeException
+{
+}
