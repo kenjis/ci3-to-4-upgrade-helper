d2ab888ee3f2a303dc942fbcfe35181ad16bf352
kenjis <kenji.uui@gmail.com>
2021-01-15 14:05:35 +0900

    feat: add CI_Controller

---
+++ src/CI3Compatible/Core/CI_Controller.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\CI3Compatible\Core;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+require __DIR__ . '/get_instance.php';
+require __DIR__ . '/Common.php';
+
+class CI_Controller extends BaseController
+{
+    /** @var CI_Controller */
+    private static $instance;
+
+    /** @var CI_Loader */
+    public $load;
+
+    public function __construct()
+    {
+        self::$instance =& $this;
+
+        $this->load = CI_Loader::getInstance();
+        $this->load->setController($this);
+    }
+
+    public function initController(
+        RequestInterface $request,
+        ResponseInterface $response,
+        LoggerInterface $logger
+    ): void {
+        parent::initController($request, $response, $logger);
+    }
+
+    public static function &get_instance(): CI_Controller
+    {
+        return self::$instance;
+    }
+}
