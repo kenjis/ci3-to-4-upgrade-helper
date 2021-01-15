0ecf7e76d8b1991cb0e5b528deaa6e51103780fc
kenjis <kenji.uui@gmail.com>
2021-01-15 13:45:44 +0900

    feat: add exception classes

---
+++ src/CI3Compatible/Exception/NotImplementedException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\CI3Compatible\Exception;
+
+/**
+ * Exception for Not Implemented Yet
+ *
+ * This function will be implemented, but not yet implemented.
+ */
+class NotImplementedException extends LogicException
+{
+}
---
+++ src/CI3Compatible/Exception/NotSupportedException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\CI3Compatible\Exception;
+
+/**
+ * Exception for Not Supported
+ *
+ * This function is not supported. Please migrate it manually.
+ */
+class NotSupportedException extends LogicException
+{
+}
