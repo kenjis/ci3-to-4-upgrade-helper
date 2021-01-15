79fdb0e66193fe63e7b900cdd34aecdec228b035
kenjis <kenji.uui@gmail.com>
2021-01-15 14:04:57 +0900

    feat: add CI_Model

---
+++ src/CI3Compatible/Core/CI_Model.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\CI3Compatible\Core;
+
+class CI_Model
+{
+    public function __get($key)
+    {
+        // Debugging note:
+        //  If you're here because you're getting an error message
+        //  saying 'Undefined Property: system/core/Model.php', it's
+        //  most likely a typo in your model code.
+        return get_instance()->$key;
+    }
+}
