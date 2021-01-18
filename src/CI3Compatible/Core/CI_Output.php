78f2a97c4d71a9f83a1daaa942b5383914afc31f
kenjis <kenji.uui@gmail.com>
2021-01-18 16:23:31 +0900

    feat: add CI_Output::set_header()

--- src/CI3Compatible/Core/CI_Output.php
+++ src/CI3Compatible/Core/CI_Output.php
@@ -4,7 +4,41 @@ declare(strict_types=1);
 
 namespace Kenjis\CI3Compatible\Core;
 
+use Config\Services;
+
+use function explode;
+use function trim;
+
 class CI_Output
 {
-    // @TODO
+    /**
+     * Set Header
+     *
+     * Lets you set a server header which will be sent with the final output.
+     *
+     * Note: If a file is cached, headers will not be sent.
+     *
+     * @param   string $header  Header
+     * @param   bool   $replace Whether to replace the old header value, if already set
+     *
+     * @return  CI_Output
+     *
+     * @todo    We need to figure out how to permit headers to be cached.
+     */
+    public function set_header(string $header, bool $replace = true): self
+    {
+        $response = Services::response();
+
+        [$name, $value] = explode(':', $header, 2);
+
+        if ($replace) {
+            $response->setHeader($name, trim($value));
+
+            return $this;
+        }
+
+        $response->appendHeader($name, trim($value));
+
+        return $this;
+    }
 }
---
+++ tests/CI3Compatible/Core/CI_OutputTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\CI3Compatible\Core;
+
+use Config\Services;
+use Kenjis\CI3Compatible\TestCase;
+
+class CI_OutputTest extends TestCase
+{
+    /** @var CI_Output */
+    private $output;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->output = new CI_Output();
+    }
+
+    public function test_set_header(): void
+    {
+        $this->output->set_header('Content-Type: text/html; charset=Shift_JIS');
+
+        $response = Services::response();
+        $this->assertSame(
+            'Content-Type: text/html; charset=Shift_JIS',
+            (string) $response->header('Content-Type')
+        );
+    }
+
+    public function test_set_header_append(): void
+    {
+        $this->output->set_header('Accept: json');
+        $this->output->set_header('Accept: xml', false);
+
+        $response = Services::response();
+        $this->assertSame(
+            'Accept: json, xml',
+            (string) $response->header('Accept')
+        );
+    }
+
+    public function test_set_header_replace(): void
+    {
+        $response = Services::response();
+        $response->removeHeader('Cache-Control');
+
+        $this->output->set_header('Cache-Control: no-cache');
+        $this->output->set_header('Cache-Control: cache');
+
+        $this->assertSame(
+            'Cache-Control: cache',
+            (string) $response->header('Cache-Control')
+        );
+    }
+}
