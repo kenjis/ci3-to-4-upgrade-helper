565840a8e239af5cbc339b0e0217e30cad648d12
kenjis <kenji.uui@gmail.com>
2021-01-19 16:29:20 +0900

    refactor: extract SeederNewsTable trait

--- tests/CI3Compatible/Database/CI_DB_driverTest.php
+++ tests/CI3Compatible/Database/CI_DB_driverTest.php
@@ -4,72 +4,18 @@ declare(strict_types=1);
 
 namespace Kenjis\CI3Compatible\Database;
 
-use Config\Database;
 use Kenjis\CI3Compatible\Exception\NotSupportedException;
 
 class CI_DB_driverTest extends DatabaseTestCase
 {
+    use SeederNewsTable;
+
     /** @var CI_DB_driver */
     private $driver;
 
     public function setUp(): void
     {
         $this->driver = new CI_DB_driver(self::$connection);
-    }
-
-    protected static function createTable(): void
-    {
-        $forge = Database::forge();
-        $forge->addField(
-            [
-                'id' => [
-                    'type' => 'INT',
-                    'constraint' => 11,
-                    'unsigned' => true,
-                    'auto_increment' => true,
-                ],
-                'title' => [
-                    'type' => 'VARCHAR',
-                    'constraint' => '128',
-                    'null' => false,
-                ],
-                'slug' => [
-                    'type' => 'VARCHAR',
-                    'constraint' => '128',
-                    'null' => false,
-                ],
-                'body' => [
-                    'type' => 'TEXT',
-                    'null' => false,
-                ],
-            ]
-        );
-        $forge->addPrimaryKey('id');
-        $forge->createTable('news', true);
-    }
-
-    protected static function seedData(): void
-    {
-        self::$connection->table('news')->truncate();
-
-        $data = [
-            [
-                'title' => 'Elvis sighted',
-                'slug' => 'elvis-sighted',
-                'body' => 'Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.',
-            ],
-            [
-                'title' => 'Say it isn\'t so!',
-                'slug' => 'say-it-isnt-so',
-                'body' => 'Scientists conclude that some programmers have a sense of humor.',
-            ],
-            [
-                'title' => 'Caffeination, Yes!',
-                'slug' => 'caffeination-yes',
-                'body' => 'World\'s largest coffee shop open onsite nested coffee shop for staff only.',
-            ],
-        ];
-        self::$connection->table('news')->insertBatch($data);
     }
 
     public function test_count_all(): void
--- tests/CI3Compatible/Database/CI_DB_query_builderTest.php
+++ tests/CI3Compatible/Database/CI_DB_query_builderTest.php
@@ -4,71 +4,16 @@ declare(strict_types=1);
 
 namespace Kenjis\CI3Compatible\Database;
 
-use Config\Database;
-
 class CI_DB_query_builderTest extends DatabaseTestCase
 {
+    use SeederNewsTable;
+
     /** @var CI_DB_query_builder */
     private $queryBuilder;
 
     public function setUp(): void
     {
         $this->queryBuilder = new CI_DB_query_builder(self::$connection);
-    }
-
-    protected static function createTable(): void
-    {
-        $forge = Database::forge();
-        $forge->addField(
-            [
-                'id' => [
-                    'type' => 'INT',
-                    'constraint' => 11,
-                    'unsigned' => true,
-                    'auto_increment' => true,
-                ],
-                'title' => [
-                    'type' => 'VARCHAR',
-                    'constraint' => '128',
-                    'null' => false,
-                ],
-                'slug' => [
-                    'type' => 'VARCHAR',
-                    'constraint' => '128',
-                    'null' => false,
-                ],
-                'body' => [
-                    'type' => 'TEXT',
-                    'null' => false,
-                ],
-            ]
-        );
-        $forge->addPrimaryKey('id');
-        $forge->createTable('news', true);
-    }
-
-    protected static function seedData(): void
-    {
-        self::$connection->table('news')->truncate();
-
-        $data = [
-            [
-                'title' => 'Elvis sighted',
-                'slug' => 'elvis-sighted',
-                'body' => 'Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.',
-            ],
-            [
-                'title' => 'Say it isn\'t so!',
-                'slug' => 'say-it-isnt-so',
-                'body' => 'Scientists conclude that some programmers have a sense of humor.',
-            ],
-            [
-                'title' => 'Caffeination, Yes!',
-                'slug' => 'caffeination-yes',
-                'body' => 'World\'s largest coffee shop open onsite nested coffee shop for staff only.',
-            ],
-        ];
-        self::$connection->table('news')->insertBatch($data);
     }
 
     public function test_get_all_records(): void
