b484d4917f3284f98984a5dcb760a83145d35a73
kenjis <kenji.uui@gmail.com>
2021-01-19 10:53:02 +0900

    feat: add CI_Email::initialize()

--- src/CI3Compatible/Library/CI_Email.php
+++ src/CI3Compatible/Library/CI_Email.php
@@ -4,7 +4,84 @@ declare(strict_types=1);
 
 namespace Kenjis\CI3Compatible\Library;
 
+use CodeIgniter\Email\Email;
+use Config\Email as EmailConfig;
+use Config\Services;
+
 class CI_Email
 {
-    // @TODO
+    /** @var Email */
+    private $email;
+    private $propertyMap = [
+        // CI3 => CI4
+        'useragent' => 'userAgent',
+        'mailpath' => 'mailPath',
+        'protocol' => 'protocol',
+        'smtp_host' => 'SMTPHost',
+        'smtp_user' => 'SMTPUser',
+        'smtp_pass' => 'SMTPPass',
+        'smtp_port' => 'SMTPPort',
+        'smtp_timeout' => 'SMTPTimeout',
+        'smtp_keepalive' => 'SMTPKeepAlive',
+        'smtp_crypto' => 'SMTPCrypto',
+        'wordwrap' => 'wordWrap',
+        'wrapchars' => 'wrapChars',
+        'mailtype' => 'mailType',
+        'charset' => 'charset',
+        'alt_message' => 'altMessage',
+        'validate' => 'validate',
+        'priority' => 'priority',
+        'newline' => 'newline',
+        'crlf' => 'CRLF',
+        'dsn' => 'DSN',
+        'send_multipart' => 'sendMultipart',
+        'bcc_batch_mode' => 'BCCBatchMode',
+        'bcc_batch_size' => 'BCCBatchSize',
+    ];
+
+    /**
+     * @param EmailConfig|array|null $config
+     */
+    public function __construct($config = null)
+    {
+        $this->email = Services::email($config);
+    }
+
+    /**
+     * For debugging
+     *
+     * @return Email
+     */
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    /**
+     * Initialize preferences
+     *
+     * @param   array $config
+     *
+     * @return  CI_Email
+     */
+    public function initialize(array $config = []): self
+    {
+        $config = $this->convertToCI4Config($config);
+        $this->email->initialize($config);
+
+        return $this;
+    }
+
+    private function convertToCI4Config(array $config): array
+    {
+        foreach ($config as $key => $value) {
+            if ($this->propertyMap[$key]) {
+                $key = $this->propertyMap[$key];
+            }
+
+            $newConfig[$key] = $value;
+        }
+
+        return $newConfig;
+    }
 }
---
+++ tests/CI3Compatible/Library/CI_EmailTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\CI3Compatible\Library;
+
+use Kenjis\CI3Compatible\TestCase;
+
+class CI_EmailTest extends TestCase
+{
+    public function test_initialize(): void
+    {
+        $email = new CI_Email();
+
+        $config = [
+            'protocol' => 'mail',
+            'wordwrap' => false,
+        ];
+        $email->initialize($config);
+
+        $ci4email = $email->getEmail();
+
+        $this->assertSame('mail', $ci4email->protocol);
+        $this->assertSame(false, $ci4email->wordWrap);
+    }
+}
