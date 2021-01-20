5943b042ddc0e1ca6daace52d6730feb2ce6be47
kenjis <kenji.uui@gmail.com>
2021-01-20 11:33:44 +0900

    feat: add CI_User_agent::is_mobile()

--- src/CI3Compatible/Library/CI_User_agent.php
+++ src/CI3Compatible/Library/CI_User_agent.php
@@ -4,7 +4,29 @@ declare(strict_types=1);
 
 namespace Kenjis\CI3Compatible\Library;
 
+use CodeIgniter\HTTP\UserAgent;
+use Config\Services;
+
 class CI_User_agent
 {
-    // @TODO
+    /** @var UserAgent */
+    private $agent;
+
+    public function __construct()
+    {
+        $request = Services::request();
+        $this->agent = $request->getUserAgent();
+    }
+
+    /**
+     * Is Mobile
+     *
+     * @param   string $key
+     *
+     * @return  bool
+     */
+    public function is_mobile(?string $key = null): bool
+    {
+        return $this->agent->isMobile($key);
+    }
 }
---
+++ tests/CI3Compatible/Library/CI_User_agetTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\CI3Compatible\Library;
+
+use Config\Services;
+use Kenjis\CI3Compatible\TestCase;
+
+class CI_User_agetTest extends TestCase
+{
+    public function test_(): void
+    {
+        Services::reset();
+        $_SERVER['HTTP_USER_AGENT'] = 'Mozilla/4.5 [en] (X11; U; Linux 2.2.9 i586).';
+
+        $agent = new CI_User_agent();
+
+        $ret = $agent->is_mobile();
+
+        $this->assertFalse($ret);
+    }
+}
