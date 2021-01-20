dc9b40942752a3cd1f3a7a8be05c6b58d2fad5ab
kenjis <kenji.uui@gmail.com>
2021-01-20 15:14:22 +0900

    feat: add html_escape()

--- src/CI3Compatible/Core/Common.php
+++ src/CI3Compatible/Core/Common.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use CodeIgniter\Exceptions\PageNotFoundException;
+use Kenjis\CI3Compatible\Exception\NotSupportedException;
 
 if (! function_exists('show_404')) {
     /**
@@ -11,5 +12,26 @@ if (! function_exists('show_404')) {
     function show_404(string $page = '', bool $log_error = true): void
     {
         throw new PageNotFoundException($page);
+    }
+}
+
+if (! function_exists('html_escape')) {
+    /**
+     * Returns HTML escaped variable.
+     *
+     * @param   mixed $var           The input string or array of strings to be escaped.
+     * @param   bool  $double_encode $double_encode set to FALSE prevents escaping twice.
+     *
+     * @return  mixed           The escaped string or array of strings as a result.
+     */
+    function html_escape($var, bool $double_encode = true)
+    {
+        if ($double_encode === false) {
+            throw new NotSupportedException(
+                '$double_encode = false is not supported.'
+            );
+        }
+
+        return esc($var, 'html');
     }
 }
