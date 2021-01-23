314d605fe012f5c413ef231fc50017d9d6e65910
kenjis <kenji.uui@gmail.com>
2021-01-24 08:44:22 +0900

    feat: add CI_Session::set_userdata(), userdata()

--- src/CI3Compatible/Library/CI_Session.php
+++ src/CI3Compatible/Library/CI_Session.php
@@ -4,10 +4,103 @@ declare(strict_types=1);
 
 namespace Kenjis\CI3Compatible\Library;
 
+use CodeIgniter\Session\Session;
+use Config\App;
+use Config\Services;
+use Kenjis\CI3Compatible\Exception\NotSupportedException;
+
+use function is_array;
 use function session_destroy;
 
 class CI_Session
 {
+    /** @var Session */
+    private $session;
+
+    /**
+     * @param   App|array|null $params Configuration parameters
+     *
+     * @return  void
+     */
+    public function __construct(?array $params = null)
+    {
+        if (is_array($params)) {
+            throw new NotSupportedException(
+                'Configuration array is not supported.'
+                . ' Please convert it to `Config\App` class.'
+                . ' See <https://codeigniter4.github.io/CodeIgniter4/libraries/sessions.html#session-preferences>.'
+            );
+        }
+
+        $this->session = Services::session($params);
+    }
+
+    /**
+     * For debugging
+     *
+     * @return Session
+     *
+     * @internal
+     */
+    public function getCI4Library(): Session
+    {
+        return $this->session;
+    }
+
+    /**
+     * Set userdata
+     *
+     * Legacy CI_Session compatibility method
+     *
+     * @param   mixed $data  Session data key or an associative array
+     * @param   mixed $value Value to store
+     *
+     * @return  void
+     */
+    public function set_userdata($data, $value = null)
+    {
+        $this->session->set($data, $value);
+    }
+
+    /**
+     * Userdata (fetch)
+     *
+     * Legacy CI_Session compatibility method
+     *
+     * @param   string $key Session data key
+     *
+     * @return  mixed   Session data value or NULL if not found
+     */
+    public function userdata(?string $key = null)
+    {
+        return $this->session->get($key);
+    }
+
+    /**
+     * __set()
+     *
+     * @param   string $key   Session data key
+     * @param   mixed  $value Session data value
+     *
+     * @return  void
+     */
+    public function __set(string $key, $value)
+    {
+        $this->session->set($key, $value);
+    }
+
+    /**
+     * __get()
+     *
+     * @param   string $key 'session_id' or a session data key
+     *
+     * @return  mixed
+     */
+    public function __get(string $key)
+    {
+        return $this->session->get($key);
+    }
+
     /**
      * Session destroy
      *
---
+++ tests/CI3Compatible/Library/CI_SessionTest.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\CI3Compatible\Library;
+
+use CodeIgniter\Session\Handlers\FileHandler;
+use CodeIgniter\Session\Session;
+use CodeIgniter\Test\Mock\MockSession;
+use CodeIgniter\Test\TestLogger;
+use Config\App as AppConfig;
+use Config\Logger;
+use Config\Services;
+use Kenjis\CI3Compatible\TestCase;
+
+use function array_merge;
+
+/**
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState         disabled
+ */
+class CI_SessionTest extends TestCase
+{
+    /** @var CI_Session */
+    private $session;
+
+    /** @var MockSession */
+    private $ci4session;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $_COOKIE  = [];
+        $_SESSION = [];
+
+        $this->createSession();
+    }
+
+    private function createSession()
+    {
+        $this->ci4session = $this->createCI4Session();
+        Services::injectMock('session', $this->ci4session);
+
+        $this->session = new CI_Session();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        Services::reset();
+    }
+
+    private function createCI4Session($options = []): Session
+    {
+        $defaults = [
+            'sessionDriver'            => 'CodeIgniter\Session\Handlers\FileHandler',
+            'sessionCookieName'        => 'ci_session',
+            'sessionExpiration'        => 7200,
+            'sessionSavePath'          => null,
+            'sessionMatchIP'           => false,
+            'sessionTimeToUpdate'      => 300,
+            'sessionRegenerateDestroy' => false,
+            'cookieDomain'             => '',
+            'cookiePrefix'             => '',
+            'cookiePath'               => '/',
+            'cookieSecure'             => false,
+            'cookieSameSite'           => 'Lax',
+        ];
+
+        $config    = array_merge($defaults, $options);
+        $appConfig = new AppConfig();
+        foreach ($config as $key => $c) {
+            $appConfig->$key = $c;
+        }
+
+        $session = new MockSession(new FileHandler($appConfig, '127.0.0.1'), $appConfig);
+        $session->setLogger(new TestLogger(new Logger()));
+
+        return $session;
+    }
+
+    public function test_get_ci4_session(): void
+    {
+        $this->assertSame($this->ci4session, $this->session->getCI4Library());
+    }
+
+    public function test_set_userdata(): void
+    {
+        $this->session->set_userdata('some_name', 'some_value');
+
+        $this->assertSame('some_value', $_SESSION['some_name']);
+    }
+
+    public function test_set_userdata_magic_setter(): void
+    {
+        $this->session->some_name = 'some_value';
+
+        $this->assertSame('some_value', $_SESSION['some_name']);
+    }
+
+    public function test_userdata_magic_getter(): void
+    {
+        $this->session->set_userdata('some_name', 'some_value');
+
+        $this->assertSame('some_value', $this->session->some_name);
+    }
+}
