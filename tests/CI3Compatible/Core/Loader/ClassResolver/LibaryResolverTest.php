9fae1e3db018e32ec3cb6971503e0e53dea696e7
kenjis <kenji.uui@gmail.com>
2021-01-24 09:46:14 +0900

    feat: add functionality to resolve user library name

--- tests/CI3Compatible/Core/Loader/ClassResolver/LibaryResolverTest.php
+++ tests/CI3Compatible/Core/Loader/ClassResolver/LibaryResolverTest.php
@@ -9,12 +9,21 @@ use Kenjis\CI3Compatible\TestCase;
 
 class LibaryResolverTest extends TestCase
 {
-    public function test_libary_name_is_string(): void
+    public function test_resolve_ci3_library_name(): void
     {
         $resolver = new LibraryResolver();
 
         $classname = $resolver->resolve('form_validation');
 
         $this->assertSame(CI_Form_validation::class, $classname);
+    }
+
+    public function test_resolve_user_library_name(): void
+    {
+        $resolver = new LibraryResolver();
+
+        $classname = $resolver->resolve('twig');
+
+        $this->assertSame('App\\Libraries\Twig', $classname);
     }
 }
