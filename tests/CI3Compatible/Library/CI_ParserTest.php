9bfe8caa3ec81485792563398cb081eb7d7ddade
kenjis <kenji.uui@gmail.com>
2021-01-27 11:01:37 +0900

    test: add tests for CI_Parser

--- tests/CI3Compatible/Library/CI_ParserTest.php
+++ tests/CI3Compatible/Library/CI_ParserTest.php
@@ -17,7 +17,7 @@ class CI_ParserTest extends TestCase
         $this->parser = new CI_Parser($viewPath);
     }
 
-    public function test_parse_string(): void
+    public function test_parse_string_return(): void
     {
         $template = 'Hello, {firstname} {lastname}';
         $data = [
@@ -29,6 +29,20 @@ class CI_ParserTest extends TestCase
 
         $expected = 'Hello, John Doe';
         $this->assertEquals($expected, $output);
+    }
+
+    public function test_parse_string_not_return(): void
+    {
+        $expected = 'Hello, John Doe';
+        $this->expectOutputString($expected);
+
+        $template = 'Hello, {firstname} {lastname}';
+        $data = [
+            'title' => 'Mr',
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+        ];
+        $this->parser->parse_string($template, $data);
     }
 
     public function test_parse_string_variable_pair(): void
@@ -49,7 +63,7 @@ class CI_ParserTest extends TestCase
         $this->assertEquals($expected, $output);
     }
 
-    public function test_parse()
+    public function test_parse_return()
     {
         $data = [
             'blog_title' => 'My Blog Title',
@@ -79,6 +93,55 @@ class CI_ParserTest extends TestCase
 </body>
 </html>
 ';
+        $this->assertEquals($expected, $output);
+    }
+
+    public function test_parse_not_return()
+    {
+        $expected = '<html>
+<head>
+    <title>My Blog Title</title>
+</head>
+<body>
+<h3>My Blog Heading</h3>
+
+
+<h5>Title 1</h5>
+<p>Body 1</p>
+
+<h5>Title 2</h5>
+<p>Body 2</p>
+
+
+</body>
+</html>
+';
+        $this->expectOutputString($expected);
+
+        $data = [
+            'blog_title' => 'My Blog Title',
+            'blog_heading' => 'My Blog Heading',
+            'blog_entries' => [
+                ['title' => 'Title 1', 'body' => 'Body 1'],
+                ['title' => 'Title 2', 'body' => 'Body 2'],
+            ],
+        ];
+        $this->parser->parse('parser/blog_template', $data);
+    }
+
+    public function test_set_delimiters(): void
+    {
+        $this->parser->set_delimiters('{{', '}}');
+
+        $template = 'Hello, {{firstname}} {lastname}';
+        $data = [
+            'title' => 'Mr',
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+        ];
+        $output = $this->parser->parse_string($template, $data, true);
+
+        $expected = 'Hello, John {lastname}';
         $this->assertEquals($expected, $output);
     }
 }
