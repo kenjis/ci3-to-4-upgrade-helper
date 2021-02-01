0c127d4e955ab1d7fefd8ede4b9042f4b4ed9940
kenjis <kenji.uui@gmail.com>
2021-02-01 15:52:39 +0900

    refactor: use kenjis/phpunit-helper

--- tests/CI3Compatible/TestCase.php
+++ tests/CI3Compatible/TestCase.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Kenjis\CI3Compatible;
 
+use Kenjis\PhpUnitHelper\ReflectionHelper;
+use Kenjis\PhpUnitHelper\TestDouble;
+
 class TestCase extends \PHPUnit\Framework\TestCase
 {
-    use TestDoubleTrait;
-    use ReflectionHelperTrait;
+    use TestDouble;
+    use ReflectionHelper;
     use LogTestHelperTrait;
 }
