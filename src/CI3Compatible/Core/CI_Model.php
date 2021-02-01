3298e1c498ccff2eb245a6fcdd3ef6a4cafa2bae
kenjis <kenji.uui@gmail.com>
2021-02-01 10:30:15 +0900

    docs: add copyright notices

--- src/CI3Compatible/Core/CI_Model.php
+++ src/CI3Compatible/Core/CI_Model.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * Copyright (c) 2021 Kenji Suzuki
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/kenjis/ci3-to-4-upgrade-helper
+ */
+
 namespace Kenjis\CI3Compatible\Core;
 
 class CI_Model
