fa2435812d46409068e248c72af9236d9f358958
kenjis <kenji.uui@gmail.com>
2021-02-02 15:44:16 +0900

    refactor: use Captcha::createCaptcha()

--- src/CI3Compatible/Helper/captcha_helper.php
+++ src/CI3Compatible/Helper/captcha_helper.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * @see https://github.com/kenjis/ci3-to-4-upgrade-helper
  */
 
-use Gregwar\Captcha\CaptchaBuilder;
+use Kenjis\CI3Like\Captcha\Captcha;
 
 if (! function_exists('create_captcha')) {
     /**
@@ -30,37 +30,6 @@ if (! function_exists('create_captcha')) {
         string $img_url = '',
         string $font_path = ''
     ): array {
-        $now = microtime(true);
-
-        if (isset($data['img_path'])) {
-            $img_path = $data['img_path'];
-        }
-
-        if (isset($data['img_url'])) {
-            $img_url = $data['img_url'];
-        }
-
-        $word = $data['word'] ?? null;
-        $img_filename = $now . '.png';
-        $img_id = $data['img_id'] ?? '';
-        $img_src = rtrim($img_url, '/') . '/' . $img_filename;
-        $img_width = $data['img_width'] ?? 150;
-        $img_height = $data['img_height'] ?? 30;
-        $img_alt = $data['img_alt'] ?? 'captcha';
-        $img = '<img ' . ($img_id === '' ? '' : 'id="' . $img_id . '"')
-            . ' src="' . $img_src . '" style="width: ' . $img_width
-            . 'px; height: ' . $img_height . 'px; border: 0;" alt="'
-            . $img_alt . '" />';
-
-        $builder = new CaptchaBuilder($word);
-        $builder->build();
-        $builder->save($img_path . $img_filename);
-
-        return [
-            'word' => $word ?? $builder->getPhrase(),
-            'time' => $now,
-            'image' => $img,
-            'filename' => $img_filename,
-        ];
+        return Captcha::createCaptcha($data, $img_path, $img_url, $font_path);
     }
 }
