a813ea61cfb03ba4bc6ddd459e87aa1d1dfa1b83
kenjis <kenji.uui@gmail.com>
2021-02-14 05:55:58 +0900

    test: initialize controller
    If there is no instance, test may fail when one test file is executed.

--- tests/CI3Compatible/TestCase.php
+++ tests/CI3Compatible/TestCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kenjis\CI3Compatible;
 
+use Kenjis\CI3Compatible\Core\CI_Controller;
 use Kenjis\PhpUnitHelper\ReflectionHelper;
 use Kenjis\PhpUnitHelper\TestDouble;
 
@@ -12,4 +13,10 @@ class TestCase extends \PHPUnit\Framework\TestCase
     use TestDouble;
     use ReflectionHelper;
     use LogTestHelperTrait;
+
+    public function setUp(): void
+    {
+        // Initialize controller
+        new CI_Controller();
+    }
 }
