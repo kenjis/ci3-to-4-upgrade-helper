2aaf2881110aa74aceb53fc2b7f71da0064f7108
kenjis <kenji.uui@gmail.com>
2021-02-16 10:53:55 +0900

    feat: add TestCase class for application testing

---
+++ src/CI3Compatible/Test/TestCase/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) 2021 Kenji Suzuki
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/kenjis/ci3-to-4-upgrade-helper
+ */
+
+namespace Kenjis\CI3Compatible\Test\TestCase;
+
+use CodeIgniter\Test\CIUnitTestCase;
+use Kenjis\CI3Compatible\Test\Traits\ResetInstance;
+use Kenjis\PhpUnitHelper\TestDouble;
+
+class TestCase extends CIUnitTestCase
+{
+    use ResetInstance;
+    use TestDouble;
+}
---
+++ tests/CI3Compatible/Test/TestCase/TestCaseTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kenjis\CI3Compatible\Test\TestCase;
+
+use App\Controllers\MY_Controller;
+use Kenjis\CI3Compatible\Core\CI_Controller;
+use Kenjis\CI3Compatible\Test\TestCase\TestCase as CI3TestCase;
+use Kenjis\CI3Compatible\TestCase;
+
+use function get_instance;
+
+class TestCaseTest extends TestCase
+{
+    public function test_resetInstance()
+    {
+        $testCase = new CI3TestCase();
+
+        $oldController = get_instance();
+        $testCase->resetInstance();
+        $newController = get_instance();
+
+        $this->assertNotSame($oldController, $newController);
+        $this->assertInstanceOf(CI_Controller::class, $newController);
+    }
+
+    public function test_resetInstance_MY_Controller()
+    {
+        $testCase = new CI3TestCase();
+
+        $oldController = get_instance();
+        $testCase->resetInstance(true);
+        $newController = get_instance();
+
+        $this->assertNotSame($oldController, $newController);
+        $this->assertInstanceOf(MY_Controller::class, $newController);
+    }
+}
